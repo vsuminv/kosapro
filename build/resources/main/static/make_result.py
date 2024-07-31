f78e9f7196dd87e5ca26b9819af390ae2bd11bef
ruukr8080@gmail.com <ruukr8080@gmail.com>
2024-07-31 11:32:44 +0900

    버튼 6개 생성 a태그로 이동. 나중에 th로 변경

--- build/resources/main/static/make_result.py
+++ build/resources/main/static/make_result.py
@@ -7,13 +7,13 @@ from openpyxl import load_workbook  # 엑셀 파일 처리 모듈(정렬, 색채
 from openpyxl.styles import Alignment, PatternFill, Font, Border, Side
 
 # JSON 파일의 경로 찾기
-json_files = glob.glob('C:/Spring/project/KosaProject/src/main/resources/result2.json')
+json_files = glob.glob('C:/Spring/project/saProject/src/main/resources/result2.json')
 
 data = {} # 진단 데이터(json)
 df = {}   # 진단 데이터프레임
 df_info = {}
 df_results = {}
-num = 0   # 진단 서버 개수 
+num = 0   # 진단 서버 개수
 
 # 각 결과를 데이터프레임으로 저장하기
 for json_file in json_files:
--- src/main/java/exam/Kosademo/controller/HomeController.java
+++ src/main/java/exam/Kosademo/controller/HomeController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class HomeController {
     @GetMapping("/")
-    public String home(Model model) {
-        String tq = "tlqkf";
-        model.addAttribute(tq);
+    public String home() {
         return "pages/home";
     }
 
@@ -26,11 +24,4 @@ public class HomeController {
 //            List<DataRow> dataRows = model.addAttribute("dataRows", dataRows);
         return "dashboard";
     }
-
-
-    @GetMapping("/cards")
-    public String cards() {
-        return "fragment/cards";
-    }
-
 }
