811ca519c0b83879fd16d214031de80968ea75c1
ruukr8080@gmail.com <ruukr8080@gmail.com>
2024-08-12 21:17:28 +0900

    ok

--- src/main/java/exam/Kosademo/controller/S3Controller.java
+++ src/main/java/exam/Kosademo/controller/S3Controller.java
@@ -15,10 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -26,21 +29,20 @@ import java.util.List;
 public class S3Controller {
 
     private final S3Service s3;
-    private final TestService t;
-    private final HttpSession httpSession;
+    private final S3Client s3Client;
 
+    @Value("${aws.s3.bucket.name}")
+    private String bucketName;
     @Value("${centos.script.path}")
     private String centosScriptPath;
-
+    @Value("${json.file.path}")
+    private String ec2Path;
     @Value("${result.json.path}")
     private String resultJsonPath;
-
     @Value("${ssh.script.path}")
     private String scriptPath;
-
     @Value("${ssh.json.path}")
     private String jsonPath;
-
     @PostMapping("/run")
     public RedirectView start(@RequestParam("username") String username,
                               @RequestParam("password") String password,
@@ -48,60 +50,29 @@ public class S3Controller {
                               @RequestParam("port") int port,
                               RedirectAttributes redirectAttributes) {
         try {
-            s3.connecting(username, password, ipAddress, port);
             s3.runCheck(username, password, ipAddress, port, scriptPath, jsonPath);
-            s3.uploadJsonFile(s3.downloadLatestJsonFile());
-
+            if (s3.findLatestJsonFile().isEmpty()) {
+                log.info("json파일 없음 {}",ec2Path);
+                return new RedirectView();
+            }
+            File file = s3.findLatestJsonFile().get();
+            log.info("json파일 찾음 절대 경로 : {}", file.getAbsolutePath());
+            log.info("json파일 찾음 상대 경로 : {}", file.getCanonicalPath());
+            s3Client.putObject(PutObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key(file.getName())
+                            .build(),
+                    file.toPath());
+            log.info("버켓에 업로드" + bucketName);
             redirectAttributes.addFlashAttribute("successMessage", "에이전트 작업 성공");
             return new RedirectView("/result");
         } catch (JSchException | SftpException | IOException e) {
             log.error("에이전트 실행 또는 S3 업로드 중 에러 발생", e);
             redirectAttributes.addFlashAttribute("errorMessage", "에러: " + e.getMessage());
-            return new RedirectView("/");
+            return new RedirectView();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @PostMapping("/Test")
-    public RedirectView test(@RequestParam("username") String username,
-                              @RequestParam("password") String password,
-                              @RequestParam("ipAddress") String ipAddress,
-                              @RequestParam("port") int port,
-                              RedirectAttributes redirectAttributes) {
-        try {
-            log.info("CentOS6.sh 경로: {}", centosScriptPath);
-            log.info("result.json 경로: {}", resultJsonPath);
-            t.connecting(username, password, ipAddress, port);
-            t.test(username, password, port, ipAddress, centosScriptPath, resultJsonPath);
-//            t.renameJsonFile(resultJsonPath);
-//            t.renameJsonFile(resultJsonPath);
-
-            redirectAttributes.addFlashAttribute("successMessage", " 실행 성공");
-            return new RedirectView("/result");
-        } catch (JSchException e) {
-
-            log.error("에이전트 실행 또는 S3 업로드 중 에러 발생", e);
-            redirectAttributes.addFlashAttribute("errorMessage", "에러: " + e.getMessage());
-            return new RedirectView("/");
-        } catch (SftpException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-
-    //세션에서 클라로 받기 나주ㅡㅇ에하자
-    @PostMapping("/down")
-    public RedirectView down(Model model, RedirectAttributes redirectAttributes) {
-        List<String> models = new ArrayList<>();
-        model.addAttribute("username", httpSession.getAttribute("username"));
-        model.addAttribute("ipAddress", httpSession.getAttribute("ipAddress"));
-        model.addAttribute("port", httpSession.getAttribute("port"));
-        model.addAttribute("scriptPath", scriptPath);
-        model.addAttribute("jsonPath", jsonPath);
-        model.addAttribute("path", scriptPath);
-        return null;
     }
 }
 
