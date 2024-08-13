0492832d6f86a32280fecfb1d301185b4e7ae134
ruukr8080@gmail.com <ruukr8080@gmail.com>
2024-08-13 17:19:15 +0900

    dd

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,21 @@ module.exports = {
     content: [
         './src/main/resources/templates/**/*.{html,js}', // Thymeleaf 템플릿 경로
         './src/main/resources/static/**/*.{html,js}', // Static 파일 경로
+        'C:/Kosa/out/resources/templates/**/*.{html,js}', // Thymeleaf 템플릿 경로
+        "./node_modules/flowbite/**/*.js",
+        "../resources/templates/**/*.{html,js}"
     ],
     theme: {
+        colors:{
+            gray:{
+                50: '#e8e6e6',
+                55: '#b35151'
+            }
+
+        },
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
